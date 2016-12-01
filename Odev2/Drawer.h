0eecf47be1c1d781aaa883940617c99fe77a280b
yigithanbalci <yigithanbalci@users.noreply.github.com>
2016-12-01 11:06:09 +0200

    Add files via upload

---
+++ Odev2/Drawer.h
@@ -0,0 +1,145 @@
+#include <glut.h>
+#include <vector>
+#include <math.h>
+#include <string>
+#include <iostream>
+#include <windows.h>
+
+std::vector<std::pair<char, std::pair<double, double>>> pointsAndCoordinates;
+int ** GraphOpenGL;
+std::vector<std::pair<int, std::pair<int, int>>> MSTOpenGL;
+int vertexNumberOpenGL;
+
+enum GraphType {
+	cizge,
+	katliCizge,
+	sozdeCizge,
+	yonluCizge,
+	yonluKatliCizge,
+
+};
+
+GraphType graphType;
+
+void setGraphType(GraphType graph_type)
+{
+	graphType = graph_type;
+}
+
+void setPointsAndCoordinates(std::vector<std::pair<char, std::pair<double, double>>> points_and_coordinates)
+{
+	pointsAndCoordinates = points_and_coordinates;
+}
+
+void setGraph(int **Graph){
+	GraphOpenGL = Graph;
+}
+
+void setVertexNumber(int vertexNumber){
+	vertexNumberOpenGL = vertexNumber;
+}
+
+void setMST(std::vector<std::pair<int, std::pair<int, int>>> MST){
+	MSTOpenGL = MST;
+}
+
+void
+display(void)
+{
+	if (graphType == cizge){
+		glLoadIdentity();
+
+		glPointSize(5);
+		glBegin(GL_POINTS);
+		for (int i = 0; i < pointsAndCoordinates.size(); i++) {
+			glBegin(GL_POINTS);
+			glColor3f(1.0f, 1.0f, 1.0f);
+			glVertex2d(pointsAndCoordinates[i].second.first, pointsAndCoordinates[i].second.second);
+			glEnd();
+
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glRasterPos2f((pointsAndCoordinates[i].second.first) * 1.12, (pointsAndCoordinates[i].second.second) * 1.12); // <-- position of text
+
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, pointsAndCoordinates[i].first);
+
+			glColor3f(1.0f, 1.0f, 1.0f);
+		}
+
+		glLineWidth(1);
+		glEnable(GL_LINE_SMOOTH);
+		glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+		glBegin(GL_LINES);
+
+		//coulda use std::find but this is more easier and basic.
+		int first = 0;
+		for (int i = 0; i < vertexNumberOpenGL; i++) {
+			for (int k = i; k < vertexNumberOpenGL; k++){
+				if (GraphOpenGL[i][k] != 0){
+					glBegin(GL_LINES);
+					glVertex2d(pointsAndCoordinates[i].second.first, pointsAndCoordinates[i].second.second);
+					glVertex2d(pointsAndCoordinates[k].second.first, pointsAndCoordinates[k].second.second);
+
+					glEnd();
+
+					/*int w = GraphOpenGL[i][k];
+					std::cout << w;
+
+					for (char c : std::to_string(w))
+					{
+						/*glRasterPos2f(fabs((pointsAndCoordinates[i].second.first - pointsAndCoordinates[k].second.first)) * 1.12,
+							fabs((pointsAndCoordinates[i].second.second - pointsAndCoordinates[k].second.second)) * 1.12); // <-- position of text
+						glRasterPos2f(0, 0);
+
+						glutStrokeCharacter(GLUT_STROKE_ROMAN, c);
+					}*/
+				}
+			}
+		}
+		glFlush();
+
+		for (unsigned int i = 0; i < MSTOpenGL.size(); i++){
+			glLineWidth(1);
+			glEnable(GL_LINE_SMOOTH);
+			glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+			glBegin(GL_LINES);
+			glColor3f(0.0f, 1.0f, 0.0f);
+
+			Sleep(750);
+			glBegin(GL_LINES);
+			glVertex2d(pointsAndCoordinates[MSTOpenGL[i].second.first].second.first, pointsAndCoordinates[MSTOpenGL[i].second.first].second.second);
+			glVertex2d(pointsAndCoordinates[MSTOpenGL[i].second.second].second.first, pointsAndCoordinates[MSTOpenGL[i].second.second].second.second);
+
+			glEnd();
+			glFlush();
+		}
+	}
+}
+
+void
+reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, 0, h);
+	glScalef(1, -1, 1);
+	glTranslatef(0, -h, 0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void
+drawGraph()
+{
+	int argc = 1;
+	char *argv[1] = { (char*)"Something" };
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(1280, 720);
+	glutCreateWindow("Graph Draw");
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(0, 0, 0);
+	glLineWidth(3.0);
+	glutDisplayFunc(display);
+	//	glutReshapeFunc(reshape);
+	glutMainLoop();
+}
---
+++ Odev2/InputHandler.h
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+int getKruskalVertexNumber(){
+	int vertexNumber = 0;
+	std::cout << "Kac koseli cizgeyi inceleyecekseniz girin." << std::endl;
+	std::cin >> vertexNumber;
+	return vertexNumber;
+}
+
+int ** getKruskalInput(int vertexNumber){
+	int** Graph;
+	Graph = new int*[vertexNumber];
+	for (int h = 0; h < vertexNumber; h++)
+	{
+		Graph[h] = new int[vertexNumber];
+
+		for (int w = 0; w < vertexNumber; w++)
+		{
+			Graph[h][w] = 0;
+		}
+	}
+	
+	std::cout << "Lutfen her satirda bir kirisi 'kose1 kose2 agirlik'\n ex : 'a b 5' seklinde girin '- 1 for finishin'" << std::endl;
+
+	bool getInput = true;
+	while (getInput)
+	{
+		char a, b;
+		int c;
+		std::cin >> a;
+		std::cin >> b;
+		if ((a == '-' && b == '1')){
+			getInput = false;
+			break;
+		}
+		std::cin >> c;
+
+		if ((a <= 'z' && a >= 'a') && (b <= 'z' && b >= 'a')){
+			int v1 = (int)(a - 'a');
+			int v2 = (int)(b - 'a');
+			Graph[v1][v2] = c;
+			Graph[v2][v1] = c;
+		}
+		else if ((a <= 'Z' && a >= 'Z') && (b <= 'Z' && b >= 'A')){
+			int v1 = (int)(a - 'A');
+			int v2 = (int)(b - 'A');
+			Graph[v1][v2] = c;
+			Graph[v2][v1] = c;
+		}
+	}
+
+	return Graph;
+}
